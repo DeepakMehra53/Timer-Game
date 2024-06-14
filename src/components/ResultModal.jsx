3633766c37a6717b389dd3b72bee46bbbd55a674
abc <abc@gmail.com>
2024-06-14 11:10:42 +0530

    Added Portal for Your Score box

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef,useImperativeHandle,useRef } from "react"
+import { createPortal } from "react-dom"
 const ResultModal =forwardRef(function ResultModal({targetTime,remainingTime,onReset},ref){
     const userLost=remainingTime<=0
     const formattedRemainingTime=(remainingTime*1000).toFixed(2)
@@ -11,7 +12,7 @@ const ResultModal =forwardRef(function ResultModal({targetTime,remainingTime,onR
             }
         }
     })
-    return(
+    return createPortal(
         <dialog ref={dialog} className="result-modal">
           { userLost &&  <h2>You lost</h2>}
           {!userLost && <h2>Your Score:{score}</h2>}
@@ -20,7 +21,8 @@ const ResultModal =forwardRef(function ResultModal({targetTime,remainingTime,onR
             <form method="dialog " onSubmit={onReset}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById("modal")
     )
 })
 export default ResultModal;
